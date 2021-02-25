1a17ef8c22281f035b578fd5a87e166484fa01c9
tincxa <tin.mai@cxagroup.com>
2021-02-25 15:15:54 +0700

    init demo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import "./App.css";
+import FetchTopTenCoin from "./components/FetchTopTenCoin";
+
+class App extends Component {
+  state = {
+    visible: true
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <FetchTopTenCoin />
+      </div>
+    );
+  }
+}
+
+export default App;
