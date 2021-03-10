653f2937925f348eacabcf0319dc0b8abf81c5e0
tincxa <tin.mai@cxagroup.com>
2021-03-10 10:30:21 +0700

    change create-react-app to create-next-app

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+import Document from 'next/document'
+
+class MyDocument extends Document {
+    static async getInitialProps(ctx) {
+        const originalRenderPage = ctx.renderPage
+
+        ctx.renderPage = () =>
+            originalRenderPage({
+                // useful for wrapping the whole react tree
+                enhanceApp: (App) => App,
+                // useful for wrapping in a per-page basis
+                enhanceComponent: (Component) => Component,
+            })
+
+        // Run the parent `getInitialProps`, it now includes the custom `renderPage`
+        return await Document.getInitialProps(ctx)
+    }
+}
+
+export default MyDocument
